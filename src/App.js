5c09b179ef89b1cb2b0b26f3005aa4da60eea801
Sifiso Mazibuko <79691867+SPMazibuko@users.noreply.github.com>
2022-05-18 00:11:03 +0300

    Admin Login Screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-
+import Login from './components/login/Login';
 function App() {
   return (
-    <div className="App">
-      <h1>Helloooo Time to get my hands dirty with some code</h1>
-    </div>
+    <Login />
   );
 }
 
